003f60d6ac7442e2f59d6052a2078a291b831f88
Brent Gruber <brent.gruber77@gmail.com>
2021-02-04 23:12:52 -0500

    Initial commit, basic sapper front end and strapi backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a9d19ec8ae77b2913c055f595f49c7b6'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '55d0f365-b6ec-40c5-932e-e8c458424048'
+};
